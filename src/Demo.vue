5c502568ed2c85f2d6feb4ccb8931783e6b3820c
bachdx <bachdx.hut@gmail.com>
2020-02-21 04:11:30 +0700

    add demo page

---
+++ src/Demo.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div>
+      <h1>Loading</h1>
+      <Loading />
+    </div>
+  </div>
+</template>
+
+<script>
+import Loading from "./components/Loading.vue";
+
+export default {
+  components: {
+    Loading
+  }
+};
+</script>
+
+<style>
+</style>
--- src/main.js
+++ src/main.js
@@ -1,2 +1,8 @@
-import Loading from './components/Loading.vue'
-export default Loading 
+import Vue from 'vue'
+import Demo from './Demo.vue'
+
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(Demo),
+}).$mount('#app')
